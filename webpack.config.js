745213b5221ae6512eb36c4b55d0e6aa9f98ab87
Christian Todd <ct.admin@mnet-mail.de>
2021-02-19 21:50:34 +0100

    https://www.robinwieruch.de/minimal-react-webpack-babel-setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader', 'eslint-loader'],
       },
     ],
   },
   resolve: {
-    extensions: ['*', '.js'],
+    extensions: ['*', '.js', '.jsx'],
   },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
   },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
     contentBase: path.resolve(__dirname, './dist'),
+    hot: true,
   },
 };
