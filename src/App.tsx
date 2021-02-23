23b392226763a400f155d8882deae43596075aaa
Christian Todd <ct.admin@mnet-mail.de>
2021-02-23 20:53:20 +0100

    https://www.robinwieruch.de/webpack-react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 interface AppProps {
   title: string;
 }
 
-const App = ({ title }: AppProps) => <div>{title}</div>;
+const Home = () => <div>Home</div>;
+
+const About = () => <div>About</div>;
+
+const App = ({ title }: AppProps) => (
+  <div>
+    {title}
+    <p />
+    <Router>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </Router>
+  </div>
+);
 
 App.propTypes = {
   title: PropTypes.string.isRequired,
